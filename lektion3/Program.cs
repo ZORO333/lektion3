3767309761cd6baec864fa8f7d0b7f4aa94f655a
ZORO333 <53059662+ZORO333@users.noreply.github.com>
2024-08-30 10:26:13 +0200

    first commit

---
+++ lektion3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program
+{
+ static void Main()
+    {
+        Console.WriteLine("Program begin");
+ 
+        int sum = 0;
+       
+        for (int i = 0; i < 4; i++)
+        {
+                Console.WriteLine($"i = {i}");
+                sum = sum + i;
+                Console.WriteLine($"sum = {sum}");
+                if(sum == 3){
+                        Console.WriteLine("We have now the magical sum of 3");
+                }
+               
+        }
+        Console.WriteLine("Program ends");
+    }
+}
